7635c7e047767f91b163648a2f018efb7db4721b
Gabriel Fernandes <gabriel.fernandes.06@outlook.com>
2025-06-12 14:03:14 -0300

    ✨ feat: add caching support with Keyv and CacheManager in BeerStylesModule

--- apps/beer-catalog/src/modules/beer-styles/beer-styles.module.ts
+++ apps/beer-catalog/src/modules/beer-styles/beer-styles.module.ts
@@ -1,9 +1,26 @@
 import { BeerStylesService } from './beer-styles.service';
 import { BeerStylesController } from './beer-styles.controller';
 import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
+import { createKeyv, Keyv } from '@keyv/redis';
+import { CacheableMemory } from 'cacheable';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.registerAsync({
+      isGlobal: true,
+      useFactory: async () => {
+        return {
+          stores: [
+            new Keyv({
+              store: new CacheableMemory({ ttl: 60000, lruSize: 5000 }),
+            }),
+            createKeyv(process.env.REDIS_URL),
+          ],
+        };
+      },
+    }),
+  ],
   controllers: [BeerStylesController],
   providers: [BeerStylesService],
 })
