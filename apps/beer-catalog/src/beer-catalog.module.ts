7545d6860cb256a777c4f2e132dd79a43d4e7565
Gabriel Fernandes <gabriel.fernandes.06@outlook.com>
2025-06-12 14:07:49 -0300

    ✨ feat: add caching support with CacheManager and redisStore in BeerCatalogModule; remove Keyv integration from BeerStylesModule

--- apps/beer-catalog/src/beer-catalog.module.ts
+++ apps/beer-catalog/src/beer-catalog.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'libs/core';
 import { BeerStylesModule } from './modules';
 import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-store';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      useFactory: async () => ({
+        store: await redisStore({
+          url: process.env.REDIS_URL || 'redis://localhost:6379',
+          ttl: 10,
+        }),
+      }),
+    }),
     PrismaModule,
     BeerStylesModule,
   ],
