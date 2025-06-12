801a2620a88619760a6c7331c2d9c7b80bc36f06
Gabriel Fernandes <gabriel.fernandes.06@outlook.com>
2025-06-12 13:14:22 -0300

    ✨ feat: add Prisma integration and initial database schema

--- apps/beer-catalog/src/beer-catalog.module.ts
+++ apps/beer-catalog/src/beer-catalog.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BeerCatalogController } from './beer-catalog.controller';
 import { BeerCatalogService } from './beer-catalog.service';
+import { PrismaModule } from 'libs/core';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [BeerCatalogController],
   providers: [BeerCatalogService],
 })
