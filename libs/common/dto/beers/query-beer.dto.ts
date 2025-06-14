06141fc99fda792625512b8e1b2030dd9d3310bd
Gabriel Fernandes <gabriel.fernandes.06@outlook.com>
2025-06-14 12:39:46 -0300

    :recycle: refactor: update query-beer DTO to remove redundant validations and improve clarity

--- libs/common/dto/beers/query-beer.dto.ts
+++ libs/common/dto/beers/query-beer.dto.ts
@@ -13,44 +13,32 @@ export class QueryBeerDto extends PaginationQueryDto {
   @ApiPropertyOptional({
     description:
       'The alcohol by volume (ABV) percentage of the beer. Example: 5.0',
-    type: Number,
-    minimum: 0,
   })
-  @IsNumber({ maxDecimalPlaces: 1 })
   @IsOptional()
   abv?: number;
 
   @ApiPropertyOptional({
     description: 'Minimum ABV percentage of the beer. Example: 4.0',
     type: Number,
-    minimum: 0,
   })
-  @IsNumber({ maxDecimalPlaces: 1 })
   @IsOptional()
   minAbv?: number;
 
   @ApiPropertyOptional({
     description: 'Maximum ABV percentage of the beer. Example: 8.0',
-    type: Number,
-    minimum: 0,
   })
-  @IsNumber({ maxDecimalPlaces: 1 })
   @IsOptional()
   maxAbv?: number;
 
   @ApiPropertyOptional({
     description: 'The ID of the beer style. Example: 1',
-    type: Number,
   })
-  @IsInt()
   @IsOptional()
   styleId?: number;
 
   @ApiPropertyOptional({
     description: 'The ID of the brewery that produces the beer. Example: 1',
-    type: Number,
   })
-  @IsInt()
   @IsOptional()
   breweryId?: number;
 }
